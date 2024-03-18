e9838f88dc6e616856a77ea8e88cffcf2bd2d75c
Rahul Suthar <rahul@unicloudtech.com>
2024-03-18 16:40:23 +0530

    Initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {useNavigate} from 'react-router-dom';
+
+export default function MyNavBar() {
+  const navigate = useNavigate();
+  const handleNavigation = (route)=> {
+    navigate(route);
+  }
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand onClick={()=>handleNavigation('/')} style ={{cursor:'pointer'}}>Navbar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link onClick={()=>handleNavigation('/home')}>Home</Nav.Link>
+            <Nav.Link onClick={()=>handleNavigation('/login')}>Login</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
